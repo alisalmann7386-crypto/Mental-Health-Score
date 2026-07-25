d43ca7470d58b8e6467813c4c08223798440033e
alisalmann7386-crypto <alisalmann7386@gmail.com>
2026-07-25 16:10:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+
+# Load trained pipeline
+model = joblib.load("Mental_Health_Model.pkl")
+
+app = FastAPI(
+    title="Mental Health Score Predictor",
+    description="Predict Mental Health Score using Machine Learning",
+    version="1.0"
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+    request=request,
+    name="index.html"
+    )
+
+# ----------------------------
+# Input Schema
+# ----------------------------
+
+class MentalHealthInput(BaseModel):
+    Age: int
+    Gender: str
+    Grouped_country: str
+    Academic_Level: str
+    Most_Used_Platform: str
+    Purpose_Of_Use: str
+    Avg_Daily_Usage_Hours: float
+    Daily_Unlocks: int
+    Study_Hours: float
+    Physical_Activity_Hours: float
+    Sleep_Hours_Per_Night: float
+    Stress_Level: str
+
+
+
+
+# ----------------------------
+# Prediction Route
+# ----------------------------
+
+@app.post("/predict")
+def predict(data: MentalHealthInput):
+
+    df = pd.DataFrame([data.model_dump()])
+
+    prediction = model.predict(df)
+
+    return {
+        "Predicted Mental Health Score": float(prediction[0])
+    }
---
+++ static/script.js
@@ -0,0 +1,140 @@
+const form = document.getElementById("predictionForm");
+
+const resultCard = document.getElementById("result");
+
+const prediction = document.getElementById("prediction");
+
+const predictionTitle = document.getElementById("predictionTitle");
+
+const predictionText = document.getElementById("predictionText");
+
+form.addEventListener("submit", async function(e){
+
+    e.preventDefault();
+
+    resultCard.style.display = "block";
+
+    resultCard.scrollIntoView({
+        behavior:"smooth"
+    });
+
+    prediction.innerHTML="...";
+    predictionTitle.innerHTML="Analyzing";
+    predictionText.innerHTML="🧠 AI is analyzing your lifestyle...";
+
+    const data={
+
+        Age:Number(document.getElementById("Age").value),
+
+        Gender:document.getElementById("Gender").value,
+
+        Grouped_country:document.getElementById("Grouped_country").value,
+
+        Academic_Level:document.getElementById("Academic_Level").value,
+
+        Most_Used_Platform:document.getElementById("Most_Used_Platform").value,
+
+        Purpose_Of_Use:document.getElementById("Purpose_Of_Use").value,
+
+        Avg_Daily_Usage_Hours:Number(document.getElementById("Avg_Daily_Usage_Hours").value),
+
+        Daily_Unlocks:Number(document.getElementById("Daily_Unlocks").value),
+
+        Study_Hours:Number(document.getElementById("Study_Hours").value),
+
+        Physical_Activity_Hours:Number(document.getElementById("Physical_Activity_Hours").value),
+
+        Sleep_Hours_Per_Night:Number(document.getElementById("Sleep_Hours_Per_Night").value),
+
+        Stress_Level:document.getElementById("Stress_Level").value
+
+    };
+
+    try{
+
+        const response=await fetch("/predict",{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify(data)
+
+        });
+
+        if(!response.ok){
+
+            throw new Error("Prediction Failed");
+
+        }
+
+        const result=await response.json();
+
+        const score=result["Predicted Mental Health Score"];
+
+        prediction.innerHTML=score.toFixed(2);
+
+        if(score>=80){
+
+            prediction.style.color="#00ff95";
+
+            predictionTitle.innerHTML="😊 Excellent";
+
+            predictionText.innerHTML=
+            "Your mental health score is excellent. Maintain your healthy routine, continue exercising, sleeping well, and balancing your digital lifestyle.";
+
+        }
+
+        else if(score>=60){
+
+            prediction.style.color="#7CFC00";
+
+            predictionTitle.innerHTML="🙂 Good";
+
+            predictionText.innerHTML=
+            "Your mental health appears to be good. Small improvements in sleep and reducing social media usage can make it even better.";
+
+        }
+
+        else if(score>=40){
+
+            prediction.style.color="#FFD700";
+
+            predictionTitle.innerHTML="😐 Moderate";
+
+            predictionText.innerHTML=
+            "Your mental health score is moderate. Consider reducing screen time, improving sleep habits, and spending more time on physical activity.";
+
+        }
+
+        else{
+
+            prediction.style.color="#ff4d4d";
+
+            predictionTitle.innerHTML="⚠ Needs Attention";
+
+            predictionText.innerHTML=
+            "Your predicted score is low. Try limiting excessive social media use, maintaining regular sleep, exercising daily, and seeking professional support if needed.";
+
+        }
+
+    }
+
+    catch(error){
+
+        prediction.innerHTML="Error";
+
+        prediction.style.color="#ff4d4d";
+
+        predictionTitle.innerHTML="Prediction Failed";
+
+        predictionText.innerHTML=
+        "Unable to connect to the FastAPI server. Please ensure the backend is running.";
+
+        console.log(error);
+
+    }
+
+});
